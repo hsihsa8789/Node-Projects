616bb32398f6c04b0807cee0f5dd3af9c680bc1f
Ashiah kumar <ashish.dubey8789@gmail.com>
2022-12-22 23:49:31 +0530

    myadd

---
+++ js/arrays.js
@@ -0,0 +1,38 @@
+// Array injava-> array can store any data elements 
+let cars = ["BMW","Audi","TATA","MAHINDRA",1,2,3,4,true];
+console.log(cars);
+
+console.log(cars[0]);
+console.log(cars[1]);
+
+cars[0] = "Ashish";
+console.log(cars);
+
+// Arrays methods in js
+
+cars.pop();
+console.log(cars);
+
+cars.push("BMW");
+console.log(cars);
+
+cars.shift();
+console.log(cars);
+
+cars.unshift("BMB");
+console.log(cars);
+
+// 2Darray in js
+
+let twoDArrays = [
+    [1,2,3],
+    [4,5,6],
+    [3,2,4]
+]
+
+console.log(twoDArrays);
+console.table(twoDArrays);
+
+
+
+
