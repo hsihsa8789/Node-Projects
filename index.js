46cd5282ce249390478c2f79ad6c1967aa88645c
Ashiah kumar <ashish.dubey8789@gmail.com>
2022-12-22 16:19:07 +0530

    jssomechanges

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ var a;
 a=10;
 console.log(a);
 console.log(typeof a);
+var num = 200;
+console.log(`half of ${num} is ${num/2}`);
+var r = "hello";
+console.log(r);
+var r = 100;
+console.log(r);
+
+let l = 100;
+console.log(l);
+// Identifier 'l' has already been declared
+// let l = "200";
+// console.log(l);
+l=300;
+console.log(l);
+
