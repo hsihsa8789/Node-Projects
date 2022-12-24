27e256f27d4a818f5f44655451bfc08bc31a6d24
Ashiah kumar <ashish.dubey8789@gmail.com>
2022-12-24 17:57:13 +0530

    automatic

---
+++ js/automaticTypeConversion.js
@@ -0,0 +1,12 @@
+var ans = "5"+1;
+console.log(ans);
+var ans = 1+"5";
+console.log(ans);
+
+console.log(null * 5);
+console.log(null * "5");
+console.log(undefined * 5);
+
+console.log("5"-1);
+
+console.log("ten"*3);
