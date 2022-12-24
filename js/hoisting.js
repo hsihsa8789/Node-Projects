75ba77167ff9a9ce270c8fe02601cb4898361a5d
Ashiah kumar <ashish.dubey8789@gmail.com>
2022-12-24 18:13:05 +0530

    hoisting

---
+++ js/hoisting.js
@@ -0,0 +1,16 @@
+console.log(a);
+greet();
+console.log(hello);
+//hello();
+var a = 10;
+
+// function
+function greet(){
+    console.log("hello guy I am good person");
+}
+// function expression
+
+var hello = function (){
+    console.log("you guys rock");
+}
+hello();
