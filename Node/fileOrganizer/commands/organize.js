019c21a717e7b96204cb93b65a0ac20797ae5417
Ashiah kumar <ashish.dubey8789@gmail.com>
2022-12-30 00:08:59 +0530

    complit

--- Node/fileOrganizer/commands/organize.js
+++ Node/fileOrganizer/commands/organize.js
@@ -35,10 +35,57 @@ function organize(srcPath){
     // 4. traverse over all the files and classify then on the basis of their extension(pdf,mp3)
 
     for(let i=0;i<allFiles.length;i++){
-        let ext = allFiles[i].split(".")[1];
+        // let ext = allFiles[i].split(".")[1];
         // let ext = path.extname(allFiles[i]);
-        console.log(ext);
+        // extname return the extension of the file
+        let fullPathOfFile = path.join(srcPath,allFiles[i]);
+        console.log(fullPathOfFile);
+        // 1. check if it is a file or folder
+        // lstatync give the information regarding the link provided basically it check is it file or folder
+        let isFile = fs.lstatSync(fullPathOfFile).isFile();
+
+        if(isFile){
+            // 1.1 get ext name
+            let ext = path.extname(allFiles[i]).split(".")[1];
+            // console.log(ext);
+            // 1.2 get forlder name  from extension
+
+            let folderName = getFolderName(ext);
+            // 1.3 copy from src folder (srcPath) and paste in destination folder (folder_name e.g doucument, media etc)
+
+            copyFileToDest(srcPath,fullPathOfFile,folderName);
+        }
     }
 }
 let srcPath = "C:/Users/SANJU/Desktop/GITDEMO/Node/fileOrganizer/download";
 organize(srcPath);
+
+function getFolderName(ext){
+    for(let key in types){
+        // console.log(key);
+        for(let i=0;i<types[key].length;i++ ){
+            if(types[key][i] == ext){
+                return key;
+            }
+        }
+    }
+}
+
+function copyFileToDest(srcPath,fullPathOfFile,folderName){
+    // 1 folder ka path banana hai
+    let destFolderPath = path.join(srcPath,"organized_files",folderName); // ...../downloads/organized_files/archives
+    console.log(destFolderPath);
+    // 2 check folder if exists ,if it does not, then make folder
+
+    if(!fs.existsSync(destFolderPath)){
+        fs.mkdirSync(destFolderPath);
+    }
+    // 3.copy file from src folder to dest folder
+
+    // returns the last portion of a path
+    let fileName = path.basename(fullPathOfFile);
+    let desFileNmae = path.join(destFolderPath,fileName);
+
+    fs.copyFileSync(fullPathOfFile,desFileNmae);
+
+}
