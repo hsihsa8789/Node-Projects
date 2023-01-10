bfceb16898d3cf61f831bbbf61f433266dc28f55
Ashiah kumar <ashish.dubey8789@gmail.com>
2023-01-10 18:21:08 +0530

    cssisalmote

--- Node/JIRA/script.js
+++ Node/JIRA/script.js
@@ -1,5 +1,11 @@
 const addBtn = document.querySelector(".add-btn");
 const nodeCont = document.querySelector(".modal-cont");
+const allPriorityColors = document.querySelectorAll(".priority-color");
+let colors = ['lightpink','lightgreen','lightblue','black'];
+let modalPriorityColor = colors[colors.length-1];
+let textAreaCont = document.querySelector(".textarea-cont");
+const mainCont = document.querySelector(".main-cont");
+
 
 let isModalPresent = false;
 addBtn.addEventListener('click',function(){
@@ -12,14 +18,39 @@ addBtn.addEventListener('click',function(){
     isModalPresent = !isModalPresent;
 })
 
-const allPriorityColors = document.querySelectorAll(".priority-color");
-
 allPriorityColors.forEach(function (colorElement){
     colorElement.addEventListener("click",function(){
         allPriorityColors.forEach(function(priorityColorElem){
             priorityColorElem.classList.remove("active");
         });
         colorElement.classList.add("active");
+        modalPriorityColor = colorElement.classList[0];
     });
 });
-// allPriorityColors
+
+nodeCont.addEventListener("keydown",function(e){
+    let key = e.key;
+    if(key=="Shift"){
+        // console.log(modalPriorityColor);
+        // console.log(textAreaCont.value);
+        createTickate(modalPriorityColor,textAreaCont.value);
+        nodeCont.style.display = "none";
+        isModalPresent = false;
+        textAreaCont.value = "";
+        allPriorityColors.forEach(function (colorElem){
+            colorElem.classList.remove("active");
+        })
+        // allPriorityColors[allPriorityColors.length-1].classList.add("action");
+        
+    }
+});
+
+function createTickate(tickateColor,data){
+    let ticketCont = document.createElement("div");
+    ticketCont.setAttribute("class","ticket-cont");
+    ticketCont.innerHTML = `<div class="ticket-color ${tickateColor}"></div>
+    <div class="ticket-id"></div>
+    <div class="task-area">${data}</div>`;
+
+    nodeCont.appendChild(ticketCont);
+}
